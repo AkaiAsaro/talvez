564b38a20f3647b25120240490c72ac22cddf891
Luiz Augusto <162390772+AkaiAsaro@users.noreply.github.com>
2024-07-22 18:50:57 -0300

    Add files via upload

---
+++ aulaVrhees/atv/main.js
@@ -0,0 +1,24 @@
+const livros =[
+    {
+        "nome": "jojo kymiou na bouken",
+        "autor": "hioriko Araki",
+        "ano de publicação": 1987,
+         "keys": ["fantasia", "aventura"]
+       },
+       
+       {
+           "nome": "as crônicas de gelo e fogo: A dança dos Dragões",
+           "autor": "George R. R. Martin",
+           "ano de publicação": 2011,
+            "keys": ["fantasia", "aventura", "dragões"]
+          },
+       
+       
+          {
+           "nome": "o silêncio dos inocentes",
+           "autor": "Thomas Harris",
+           "ano de publicação": 1988,
+            "keys": ["suspense", "terror", "mistério"]
+          }    
+    
+] 
